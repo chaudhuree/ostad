7a20c2f431401c1b3b27600d2bc6411d515255b4
chaudhuree <chaudhuree00@gmail.com>
2022-11-04 23:05:06 +0600

    use strict mode

---
+++ day02/useStrict.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// myName()
+// function myName() {
+//     name="chaudhuree"
+// }
+//throw and error because we have used strict mode
+//if we don't use use strict then we will not get any error in the terminal
+
+// use strict mode catches any type of major error and also help to solve some error that can be solved by itself
+
+myName();
+function myName() {
+  const name = "chaudhuree";
+    console.log(name)
+}
