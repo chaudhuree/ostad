96757a844def221e58a1b94492762ac6439e7e70
chaudhuree <chaudhuree00@gmail.com>
2022-11-05 23:35:05 +0600

    for of and for in loop

---
+++ day03/forIn.js
@@ -0,0 +1,7 @@
+const person = {
+  name: "chaudhuree",
+  age: 27,
+};
+for(const item in person){
+    console.log(`${item}:${person[item]}`)
+}
---
+++ day03/forOf.js
@@ -0,0 +1,4 @@
+const arr = ['a','b','c']
+for(const item of arr){
+    console.log({item})
+}
