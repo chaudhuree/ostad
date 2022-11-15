c6252487d0d49203e9ee4d74142f6d79f6bb6572
chaudhuree <chaudhuree00@gmail.com>
2022-11-15 16:47:59 +0600

    server with fs module basic

---
+++ day10/serverWithFsModule.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const data = fs.readFileSync("./index.html");
+  if ((url ="/")) {
+      res.writeHead(200)
+    res.write(data);
+    res.end();
+  } else {
+      res.writeHead(500)
+    res.write("not working");
+    res.end();
+  }
+});
+
+server.listen(5050, () => {
+  console.log("server is running on port no: 5050");
+});
