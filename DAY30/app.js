4cfc75a36bbb3a26d3c3d988fd9c48cfbcefa25f
chaudhuree <chaudhuree00@gmail.com>
2022-12-07 19:50:26 +0600

    docs: json response

--- DAY30/app.js
+++ DAY30/app.js
@@ -1,27 +1,50 @@
-var express=require('express');
+var express = require('express');
 
-app=express();
+app = express();
 
 //Simple String Response
 // res.send()--> response Body
 // res.end()---> reponse ending point...
-app.get("/one",function (req,res) {
-    res.end("This is simple string response ");
+app.get("/one", function (req, res) {
+  res.end("This is simple string response ");
 });
 
-app.post("/two",function (req,res) {
-    res.end("This is simple string response ");
+app.post("/two", function (req, res) {
+  res.end("This is simple string response ");
 });
 
 
 
 // Response Status Code
-app.get("/three",function (req,res) {
-    res.status(201).end();
+app.get("/three", function (req, res) {
+  res.status(201).end();
+})
+
+// JSON Response
+app.get("/four", function (req, res) {
+  let MyJSONArray = [
+    {
+      name: "Rabbil",
+      city: "Dhaka",
+      occupation: "Engr."
+    },
+    {
+      name: "Rakib",
+      city: "Dhaka",
+      occupation: "Pharama"
+    },
+    {
+      name: "Rifat",
+      city: "Rangpur",
+      occupation: "Student"
+    }
+
+  ]
+  res.json(MyJSONArray);
 })
 
 
-
-app.listen(8000,function () {
-    console.log("Server Run Success")
+app.listen(8000, function () {
+  console.log("Server Run Success")
 })
+
