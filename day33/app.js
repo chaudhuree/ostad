cd8d7f56206355de9d677117d8969feb1f3a1a6e
chaudhuree <chaudhuree00@gmail.com>
2022-12-08 16:33:38 +0600

    docs: json data

--- day33/app.js
+++ day33/app.js
@@ -3,5 +3,18 @@ const express = require('express');
 app = express();
 
 // middlewares ,upload , multipart form data
+app.use(express.json());
+
+// handle json data
+app.post('/', function (req,res) {
+  // send this json data through postman
+  // {
+  //   "name": "sohan",
+  //   "description": "lorem ipsum dolor sit am"
+  // }
+  let name=req.body['name']      
+  let description=req.body['description']      
+  res.send(name+" "+description)
+});
 
 app.listen(5000)
